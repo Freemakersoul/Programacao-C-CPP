dd320b87c9f852e6db447925121d8f0dff202fe6
Freemakersoul <freemakersoul@gmail.com>
2024-12-02 16:57:14 +0000

    02/12/2024

---
+++ 02/12/2024/CPP_TP_01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numero;
+
+    cout << "Digite um numero inteiro: ";
+    cin >> numero;
+    cout << "O numero introduzido foi: " << numero << endl;
+
+    return 0;
+}
